0d7313daffd402fcea2c6fc517415c796effa9cc
Andres Longueira <andreslongueirarengifo@gmail.com>
2022-10-10 10:16:43 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var excuses = (document.getElementById("excuse").innerHTML = createExcuses(
+    who,
+    what,
+    when
+  ));
 };
+
+function createExcuses(who, what, when) {
+  return (
+    who[Math.floor(Math.random() * 4 + 0)] +
+    " " +
+    what[Math.floor(Math.random() * 4 + 0)] +
+    " " +
+    when[Math.floor(Math.random() * 5 + 0)]
+  );
+}
